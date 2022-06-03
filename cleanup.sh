3c415a06033cccb9488c470815be559e7b807b45
Gaurav Singh <gauravnumber@gmail.com>
2022-06-03 11:23:03 +0530

    now given directory also put in cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -4,9 +4,20 @@ date=$(date +%d\ %b\ %Y)
 cleanup="Cleanup $date"
 dir="Cleanup/$cleanup"
 
-mkdir -p "$dir"
+main (){
+  if [[ $# -eq 1 ]]; then
+    mv -n "$1" "$dir"
+    exit 1
+  fi
+
+  mkdir -p "$dir"
 
 # mv -vu *.html *.css *.js *.json *.sh "$dir"
-find -maxdepth 1 -type f -print -exec mv '{}' "$dir" \; 
+# find -maxdepth 1 -type f -print -exec mv '{}' "$dir" \; 
 
-echo -e "Transfer complete"
+fd --exclude={.rcloneignore,.rsyncignore} --max-depth 1 --type f --exec-batch mv '{}' "$dir" \;
+
+# echo -e "Transfer complete"
+}
+
+main "$@"
